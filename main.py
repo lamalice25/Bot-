2a81d17aac0ce051f6b2c3c49bb822f733c40117
lamalice25 <titijiyan974@gmail.com>
2024-04-02 14:41:25 +0400

    C1

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
-commandes = input('Dite moi bonjour : ')
+
+enter  = input('Enter si vous voulais - , + , x : ')
+
+if 'x' in enter:
+    y1_multiplier = int(input('Entrer un chiffre le premier chiffre a multiplier : '))
+    y2_multiplier = int(input('Enter le deuxième chiffre a multiplier : '))
+
+    x_multiplier = y1_multiplier*y2_multiplier
+
+    print(x_multiplier)
+
+elif '+' in enter:
+    y1_aditionner = int(input("Enter le premier chiffre a aditionner : "))
+    y2_aditionner = int(input("Enter le deuxième chiffre a aditionner : "))
 
 
-if 'bonjour' in commandes:
-    print('Bonjour !! ')
+    x_aditionner = y1_aditionner+y2_aditionner
+
+    print(x_aditionner)
+
+elif '-' in enter:
+    y1_soustraire = int(input("Entrer le premier chiffre a soustraire : "))
+    y2_soustraire = int(input("Entrer le deuxième chiffre a soustraire : "))
+
+    x_soustraire = y1_soustraire-y2_soustraire
+
+    print(x_soustraire)
+
+
+elif 'cls' in enter:
+    running = False
+
+else:
+    print('Aucun resultat ')
